c721a3b61e218d46ab623ca53e8bcc68476fa758
Daniel Burnley <dan@danops.uk>
2018-07-26 16:34:48 +0100

    Add basic test on return form

---
+++ src/Components/ReturnForm/returnForm.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReturnForm from '.'
+import {mount} from 'enzyme'
+
+describe('<ReturnForm>', () => {
+  it('Calls the onsubmit function when submitted', () => {
+    let submitSpy = jest.fn()
+    let wrapper = mount(<ReturnForm onSubmit={submitSpy} />)
+    wrapper.find('button').simulate('submit')
+    expect(submitSpy).toHaveBeenCalled()
+  })
+})
+
