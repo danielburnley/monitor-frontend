4bab0710a80da4a55f829b67046ab225416ce733
Daniel Burnley <dan@danops.uk>
2018-08-03 10:30:50 +0100

    Add method to return gateway

--- src/Gateway/ReturnGateway/index.js
+++ src/Gateway/ReturnGateway/index.js
@@ -1,35 +1,54 @@
-import fetch from "isomorphic-fetch";
+import fetch from 'isomorphic-fetch';
+import Return from '../../Domain/Return';
 
 export default class ReturnGateway {
-  async findById(id) {
+  async baseReturnFor(id) {
     let rawResponse = await fetch(
-      `${process.env.REACT_APP_HIF_API_URL}return/get?id=${id}`,
+      `${process.env.REACT_APP_HIF_API_URL}project/get-base-return?id=${id}`,
       {
-        headers: { "Content-Type": "application/json" }
-      }
-    );
-    if (rawResponse.ok) {
-      let foundReturn = await rawResponse.json();
-      return { success: true, foundReturn };
-    } else {
-      return { success: false };
-    }
-  }
-  async submit(id, data) {
-    let rawResponse = await fetch(
-      `${process.env.REACT_APP_HIF_API_URL}return/create`,
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ project_id: id, data })
-      }
+        headers: {'Content-Type': 'application/json'},
+      },
     );
 
     if (rawResponse.ok) {
       let jsonResponse = await rawResponse.json();
-      return { success: true, returnId: jsonResponse.id };
+      let foundReturn = new Return(jsonResponse.data, jsonResponse.schema);
+      return {success: true, foundReturn};
     } else {
-      return { success: false };
+      return {success: false};
+    }
+  }
+
+  async findById(id) {
+    let rawResponse = await fetch(
+      `${process.env.REACT_APP_HIF_API_URL}return/get?id=${id}`,
+      {
+        headers: {'Content-Type': 'application/json'},
+      },
+    );
+    if (rawResponse.ok) {
+      let foundReturn = await rawResponse.json();
+      return {success: true, foundReturn};
+    } else {
+      return {success: false};
+    }
+  }
+
+  async submit(id, data) {
+    let rawResponse = await fetch(
+      `${process.env.REACT_APP_HIF_API_URL}return/create`,
+      {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({project_id: id, data}),
+      },
+    );
+
+    if (rawResponse.ok) {
+      let jsonResponse = await rawResponse.json();
+      return {success: true, returnId: jsonResponse.id};
+    } else {
+      return {success: false};
     }
   }
 }
--- src/Gateway/ReturnGateway/returnGateway.test.js
+++ src/Gateway/ReturnGateway/returnGateway.test.js
@@ -148,4 +148,69 @@ describe('Return Gateway', () => {
       });
     });
   });
+
+  describe('#BaseReturnFor', () => {
+    describe('Given a Return is found', () => {
+      let returnRequest, response;
+
+      describe('Example one', () => {
+        beforeEach(async () => {
+          process.env.REACT_APP_HIF_API_URL = 'http://cat.meow/';
+          returnRequest = nock('http://cat.meow')
+            .matchHeader('Content-Type', 'application/json')
+            .get('/project/get-base-return?id=1')
+            .reply(200, {data: {some: 'data'}, schema: {some: 'schema'}});
+          let gateway = new ReturnGateway();
+          response = await gateway.baseReturnFor(1);
+        });
+
+        it('Fetches the return from the API', () => {
+          expect(returnRequest.isDone()).toBeTruthy();
+        });
+
+        it('Returns the response from the api', () => {
+          let expectedReturn = new Return({some: 'data'}, {some: 'schema'});
+          expect(response.success).toEqual(true);
+          expect(response.foundReturn.data).toEqual({some: 'data'});
+          expect(response.foundReturn.schema).toEqual({some: 'schema'});
+        });
+      });
+
+      describe('Example two', () => {
+        beforeEach(async () => {
+          process.env.REACT_APP_HIF_API_URL = 'http://dog.woof/';
+          let returnRequest = nock('http://dog.woof')
+            .matchHeader('Content-Type', 'application/json')
+            .get('/project/get-base-return?id=5')
+            .reply(200, {data: {cats: 'meow'}, schema: {dogs: 'woof'}});
+          let gateway = new ReturnGateway();
+          response = await gateway.baseReturnFor(5);
+        });
+
+        it('Fetches the return from the API', () => {
+          expect(returnRequest.isDone()).toBeTruthy();
+        });
+
+        it('Returns the response from the api', () => {
+          let expectedReturn = new Return({cats: 'meow'}, {dogs: 'woof'});
+          expect(response.success).toEqual(true);
+          expect(response.foundReturn.data).toEqual({cats: 'meow'});
+          expect(response.foundReturn.schema).toEqual({dogs: 'woof'});
+        });
+      });
+    });
+
+    describe('Given a return is not found', () => {
+      it('Returns unsuccessful', async () => {
+        process.env.REACT_APP_HIF_API_URL = 'http://dog.woof/';
+        let returnRequest = nock('http://dog.woof')
+          .matchHeader('Content-Type', 'application/json')
+          .get('/project/get-base-return?id=5')
+          .reply(404);
+        let gateway = new ReturnGateway();
+        let response = await gateway.baseReturnFor(5);
+        expect(response).toEqual({success: false});
+      });
+    });
+  });
 });
