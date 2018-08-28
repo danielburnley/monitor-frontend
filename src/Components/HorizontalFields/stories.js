924847521803cc9a3c7a3a6c61df162a4e56e816
Daniel Burnley <dan@danops.uk>
2018-08-28 12:29:07 +0100

    Improve horizontal field formatting

--- src/Components/HorizontalFields/stories.js
+++ src/Components/HorizontalFields/stories.js
@@ -2,35 +2,77 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import HorizontalFields from ".";
 
-storiesOf("HorizontalFields", module).add("Default", () => {
-  let schema = {
-    type: "object",
-    title: "Summary",
-    properties: {
-      firstName: { type: "string", title: "First Name" },
-      surname: { type: "string", title: "Surname" },
-      foo: { type: "string", title: "Foo" },
-      meow: { type: "string", title: "Meow" },
-      woof: { type: "string", title: "woof" },
-      bar: { type: "string", title: "Bar" }
-    }
-  };
-  let formData = {
-    firstName: "First name",
-    surname: "Surname",
-    foo: "foo",
-    meow: "meow",
-    woof: "woof",
-    bar: "bar"
-  };
+storiesOf("HorizontalFields", module)
+  .add("Default", () => {
+    let schema = {
+      type: "object",
+      title: "Summary",
+      properties: {
+        firstName: { type: "string", title: "First Name" },
+        surname: { type: "string", title: "Surname" },
+        foo: { type: "string", title: "Foo" },
+        meow: { type: "string", title: "Meow" },
+        woof: { type: "string", title: "woof" },
+        bar: { type: "string", title: "Bar" }
+      }
+    };
+    let formData = {
+      firstName: "First name",
+      surname: "Surname",
+      foo: "foo",
+      meow: "meow",
+      woof: "woof",
+      bar: "bar"
+    };
 
-  return (
-    <HorizontalFields
-      schema={schema}
-      formData={formData}
-      onChange={formData => {
-        console.log(formData);
-      }}
-    />
-  );
-});
+    return (
+      <HorizontalFields
+        schema={schema}
+        formData={formData}
+        onChange={formData => {
+          console.log(formData);
+        }}
+      />
+    );
+  })
+  .add("Two next to each other", () => {
+    let schema = {
+      type: "object",
+      title: "Summary",
+      properties: {
+        firstName: { type: "string", title: "First Name" },
+        surname: { type: "string", title: "Surname" },
+        foo: { type: "string", title: "Foo" },
+        meow: { type: "string", title: "Meow" },
+        woof: { type: "string", title: "woof" },
+        bar: { type: "string", title: "Bar" }
+      }
+    };
+    let formData = {
+      firstName: "First name",
+      surname: "Surname",
+      foo: "foo",
+      meow: "meow",
+      woof: "woof",
+      bar: "bar"
+    };
+
+    return (
+      <div>
+        <HorizontalFields
+          schema={schema}
+          formData={formData}
+          onChange={formData => {
+            console.log(formData);
+          }}
+        />
+        <HorizontalFields
+          schema={schema}
+          formData={formData}
+          onChange={formData => {
+            console.log(formData);
+          }}
+        />
+      </div>
+    );
+  });
