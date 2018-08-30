2727f7c3d69d7bb4b3ca8046ad3e7344bacf1727
Robin Lacey <robin@madetech.com>
2018-08-30 09:50:21 +0100

    Updated schema in parent form storybook

--- src/Components/ParentForm/stories.js
+++ src/Components/ParentForm/stories.js
@@ -8,44 +8,47 @@ import { linkTo } from '@storybook/addon-links';
 storiesOf('Parent Form', module)
   .add('Default', () => {
     let schema = {
-      "title": "A registration form",
-      "description": "A simple form example.",
-      "type": "object",
-      "required": [
-        "firstName",
-        "lastName"
-      ],
-      "properties": {
-        "firstName": {
-          "type": "string",
-          "title": "First name"
-        },
-        "lastName": {
-          "type": "string",
-          "title": "Last name"
-        },
-        "age": {
-          "type": "integer",
-          "title": "Age"
-        },
-        "bio": {
-          "type": "string",
-          "title": "Bio"
-        },
-        "password": {
-          "type": "string",
-          "title": "Password",
-          "minLength": 3
-        },
-        "telephone": {
-          "type": "string",
-          "title": "Telephone",
-          "minLength": 10
+      title: "A registration form",
+      description: "A simple form example.",
+      type: "object",
+
+      properties: {
+        details: {
+          type: "object",
+          title: "Details",
+          properties: {
+            firstName: {
+              type: "string",
+              title: "First name"
+            },
+            lastName: {
+              type: "string",
+              title: "Last name"
+            },
+            age: {
+              type: "integer",
+              title: "Age"
+            },
+            bio: {
+              type: "string",
+              title: "Bio"
+            },
+            password: {
+              type: "string",
+              title: "Password",
+              minLength: 3
+            },
+            telephone: {
+              type: "string",
+              title: "Telephone",
+              minLength: 10
+            }
+          }
         }
       }
-    }
+    };
 
     return (
       <ParentForm schema={schema}/>
     )
-  })
+  });
