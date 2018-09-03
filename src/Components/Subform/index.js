f863a9945e8ec65c7da5236d0c0257a87133bf5c
Renny Fadoju <rennyvbirds@yahoo.com>
2018-09-03 16:58:32 +0100

    Save form changes to subform

--- src/Components/Subform/index.js
+++ src/Components/Subform/index.js
@@ -58,18 +58,28 @@ export default class SubForm extends React.Component {
   constructor(props) {
     super(props)
 
-    this.state = {selectedSection: Object.keys(props.schema.items.properties)[0], selectedIndex: 0}
+    this.state = {
+      formData: props.data,
+      selectedSection: Object.keys(props.schema.items.properties)[0],
+      selectedIndex: 0
+    }
   }
 
   renderSidebar() {
     let items = new GenerateSidebarItems().execute(
       this.props.schema,
-      this.props.formData
+      this.props.data
     ).items;
     return <Sidebar items={items} onItemClick={(section, index) => {
       this.setState({selectedSection: section, selectedIndex: index})
     }}/>;
   }
+
+  onFormChange = (formData) => {
+    let updatedData = [...this.state.formData]
+    updatedData[this.state.selectedIndex][this.state.selectedSection] = formData
+    this.setState({formData: updatedData})  
+  } 
 
   render() {
     return (
@@ -77,7 +87,10 @@ export default class SubForm extends React.Component {
         {this.renderSidebar()}
         <Form data-test={`${this.state.selectedSection}-form`}
               schema={this.props.schema.items.properties[this.state.selectedSection]}
-              formData={this.props.formData[this.state.selectedIndex][this.state.selectedSection]}
+              formData={this.state.formData[this.state.selectedIndex][this.state.selectedSection]}
+              onChange={({formData})=>{
+                this.onFormChange(formData)
+              }}
         />
       </div>
     )
