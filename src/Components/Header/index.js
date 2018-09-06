c40cd5ad965d4a92081ec18c57ae7a0d09a72799
Daniel Burnley <dan@danops.uk>
2018-09-06 11:06:31 +0100

    Calculate vertical height

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,9 +1,13 @@
-import './style.css'
-import Logo from './Homes_England_3282_DIGI_AW.png'
-import React from 'react';
+import "./style.css";
+import Logo from "./Homes_England_3282_DIGI_AW.png";
+import React from "react";
 
 export default class Header extends React.Component {
-    render() {
-        return <header><img src={Logo} alt="Homes England" id="logo"/></header>
-    }
+  render() {
+    return (
+      <header className="monitor-header">
+        <img src={Logo} className="logo" alt="Homes England" id="logo" />
+      </header>
+    );
+  }
 }
