3aecf9be94b4b46844959e6d854d39a6d46649ab
faissalMT <faissal@madetech.com>
2018-09-20 11:56:01 +0100

    Add support for array indexing in validation message

--- src/Components/ReturnPage/index.js
+++ src/Components/ReturnPage/index.js
@@ -132,13 +132,22 @@ export default class ReturnPage extends React.Component {
 
   decamelize = (string) => {
     let all_camelcase_word_boundaries = /(^.|[A-Z])/g;
-    return string.replace(all_camelcase_word_boundaries, (character) => " "+character.toUpperCase());
+    return string.replace(all_camelcase_word_boundaries, (character) => " "+character.toUpperCase()).substring(1);
   }
+
+  formatPathNode = (node) => {
+    if (typeof node == 'string') {
+      return this.decamelize(node)
+    }
+    if (typeof node == 'number') {
+      return "Item " + (node + 1);
+    }
+  };
 
   renderInvalidPaths = () => {
     return this.state.invalid_paths.map(path => {
       return (<span key={path}>
-        {path.map(this.decamelize).join(' → ')}<br/>
+        {path.map(this.formatPathNode).join(' → ')}<br/>
       </span>)
     })
   };
@@ -151,7 +160,7 @@ export default class ReturnPage extends React.Component {
     if (this.state.lastAction==="Submit") {
       return (
         <div className="alert alert-danger" role="alert" data-test="validationError">
-          Error: This return could not be submitted because the following fields were missing: <br/>
+          <strong>Error:</strong> This return could not be submitted because the following fields were missing: <br/>
           {this.renderInvalidPaths()}
         </div>
       );
@@ -159,7 +168,7 @@ export default class ReturnPage extends React.Component {
 
     return (
       <div className="alert alert-warning" role="alert" data-test="validationWarning">
-        Warning: You will not be able to submit this return until the following fields are filled in: <br/>
+        <strong>Warning:</strong> You will not be able to submit this return until the following fields are filled in: <br/>
         {this.renderInvalidPaths()}
       </div>
     );
