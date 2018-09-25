f954ca04e5debf3da58553de9e01750bd24dc123
faissalMT <faissal@madetech.com>
2018-09-25 15:36:12 +0100

    Prevent login attempt if email is empty

--- src/Components/GetToken/getToken.test.js
+++ src/Components/GetToken/getToken.test.js
@@ -12,6 +12,55 @@ async function updateFormField(input, value) {
 }
 
 describe('GetToken', () => {
+  it("doesn't show a disabled button when an email is entered", async () => {
+    let requestTokenSpy = {
+      execute: jest.fn()
+    };
+    let wrapper = mount(<GetToken projectId="1" requestToken={requestTokenSpy} targetUrl="http://localhost/"/>)
+
+    let email_input = wrapper.find('[data-test="email_input"]')
+    updateFormField(email_input, "bonsoir@corp")
+    let submit_button = wrapper.find('form')
+    await wait()
+    wrapper.update()
+
+    expect(wrapper.find('[data-test="disabled-submit-button"]').length).toEqual(0)
+    expect(wrapper.find('[data-test="submit-button"]').length).toEqual(1)
+  });
+
+  it("shows a disabled button when no email is entered", async () => {
+    let requestTokenSpy = {
+      execute: jest.fn()
+    };
+    let wrapper = mount(<GetToken projectId="1" requestToken={requestTokenSpy} targetUrl="http://localhost/"/>)
+
+    let email_input = wrapper.find('[data-test="email_input"]')
+    updateFormField(email_input, "")
+    let submit_button = wrapper.find('form')
+    await wait()
+    wrapper.update()
+
+    expect(wrapper.find('[data-test="disabled-submit-button"]').length).toEqual(1)
+    expect(wrapper.find('[data-test="submit-button"]').length).toEqual(0)
+  });
+
+  it("doesn't request a token when no email is entered a token", async () => {
+    let requestTokenSpy = {
+      execute: jest.fn()
+    };
+    let wrapper = mount(<GetToken projectId="1" requestToken={requestTokenSpy} targetUrl="http://localhost/"/>)
+
+    let email_input = wrapper.find('[data-test="email_input"]')
+    updateFormField(email_input, "")
+    let submit_button = wrapper.find('form')
+    submit_button.simulate('submit')
+    await wait()
+    wrapper.update()
+
+    expect(requestTokenSpy.execute).not.toHaveBeenCalled()
+    expect(wrapper.find('[data-test="disabled-submit-button"]').length).toEqual(1)
+  });
+
   it("requests a token", async () => {
     let requestTokenSpy = {
       execute: jest.fn()
@@ -26,7 +75,7 @@ describe('GetToken', () => {
     wrapper.update()
 
     expect(requestTokenSpy.execute).toHaveBeenCalledWith("cat@cathouse.com", "1", "http://localhost/")
-  })
+  });
 
   it("shows the 'sent' message when submitted", async () => {
     let requestTokenSpy = {
@@ -43,7 +92,9 @@ describe('GetToken', () => {
     wrapper.update()
 
     expect(wrapper.find('[data-test="sent_message"]').length).toEqual(1)
-  })
+    expect(wrapper.find('[data-test="disabled-submit-button"]').length).toEqual(0)
+    expect(wrapper.find('[data-test="submit-button"]').length).toEqual(0)
+  });
 
   it("does not show the 'sent' message before submitting", async () => {
     let requestTokenSpy = {
@@ -53,5 +104,5 @@ describe('GetToken', () => {
     let wrapper = mount(<GetToken projectId="1" requestToken={requestTokenSpy} targetUrl="http://localhost/"/>)
 
     expect(wrapper.find('[data-test="sent_message"]').length).toEqual(0)
-  })
+  });
 });
