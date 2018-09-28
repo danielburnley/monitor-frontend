7309df1e7050908facaf3c332a6417c2299100ab
Daniel Burnley <dan@danops.uk>
2018-09-28 11:52:00 +0100

    Add GA

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import 'core-js/es6/symbol';
 import 'core-js/es6/set';
 import 'core-js/es7/object'
 import Raven from 'raven-js'
+import ReactGA from 'react-ga';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import runtimeEnv from '@mars/heroku-js-runtime-env';
@@ -16,6 +17,11 @@ if (env.REACT_APP_SENTRY_DSN) {
   Raven.config(
     env.REACT_APP_SENTRY_DSN
   ).install();
+}
+
+if (env.REACT_APP_GA_ID) {
+  ReactGA.initialize(env.REACT_APP_GA_ID);
+  ReactGA.pageview(window.location.pathname + window.location.search);
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
