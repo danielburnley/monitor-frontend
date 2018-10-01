6bce556dc864e91eb305b2c00b3470cce4df91f9
faissalMT <faissal@madetech.com>
2018-10-01 12:12:42 +0100

    Use environment variable for support email

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,8 +1,14 @@
+import runtimeEnv from '@mars/heroku-js-runtime-env';
 import "./style.css";
 import Logo from "./Homes_England_3282_DIGI_AW.png";
 import React from "react";
 
 export default class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.env = runtimeEnv();
+  }
+
   render() {
     return (
       <header className="monitor-header">
@@ -10,7 +16,7 @@ export default class Header extends React.Component {
           <img src={Logo} className="logo" alt="Homes England" id="logo" />
         </div>
         <div className="beta-notice">
-          <strong className="beta">Beta</strong> This is a new service — your <a href="mailto:SUPPORT_EMAIL_TBC">feedback</a> will help us improve it.
+          <strong className="beta">Beta</strong> This is a new service — your <a href={"mailto:"+this.env.REACT_APP_SUPPORT_EMAIL}>feedback</a> will help us improve it.
         </div>
       </header>
     );
