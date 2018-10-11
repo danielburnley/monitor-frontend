88746ae53185ea364c8c6f0b441f2805f90ffd94
Emma Corbett <emma@madetech.com>
2018-10-11 11:34:28 +0100

    Add submit method to project gateway

--- src/Gateway/ProjectGateway/index.js
+++ src/Gateway/ProjectGateway/index.js
@@ -1,27 +1,52 @@
-import Project from '../../Domain/Project'
-import fetch from 'isomorphic-fetch'
-import runtimeEnv from '@mars/heroku-js-runtime-env';
+import Project from "../../Domain/Project";
+import fetch from "isomorphic-fetch";
+import runtimeEnv from "@mars/heroku-js-runtime-env";
 
 export default class ProjectGateway {
   constructor(apiKeyGateway) {
     this.apiKeyGateway = apiKeyGateway;
+    this.env = runtimeEnv();
   }
 
   async findById(id) {
-    let env = runtimeEnv()
+    let env = runtimeEnv();
     let rawResponse = await fetch(
       `${env.REACT_APP_HIF_API_URL}project/find?id=${id}`,
       {
-        headers: {'Content-Type': 'application/json',
-          'API_KEY': this.apiKeyGateway.getApiKey().apiKey},
-      },
+        headers: {
+          "Content-Type": "application/json",
+          API_KEY: this.apiKeyGateway.getApiKey().apiKey
+        }
+      }
     );
     if (rawResponse.ok) {
       let projectResponse = await rawResponse.json();
-      let foundProject = new Project(projectResponse.data, projectResponse.schema);
-      return {success: true, foundProject};
+      let foundProject = new Project(
+        projectResponse.data,
+        projectResponse.schema
+      );
+      return { success: true, foundProject };
     } else {
-      return {success: false};
+      return { success: false };
+    }
+  }
+
+  async submit(project_id) {
+    let response = await fetch(`${this.env.REACT_APP_HIF_API_URL}project/submit`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        API_KEY: this.apiKeyGateway.getApiKey().apiKey
+      },
+      body: JSON.stringify({
+        project_id
+      })
+    });
+    
+    if (response.ok) {
+      return { success: true };
+    } else {
+      return { success: false };
     }
   }
 }
