9f8bc29a91e186299f571281e1764912c43b955b
Christopher Parsons <mail@christopherparsons.me>
2018-10-22 14:23:08 +0100

    Add tests to check that subsequent changes are stored successfully

--- src/Components/MilestoneField/milestoneField.test.js
+++ src/Components/MilestoneField/milestoneField.test.js
@@ -220,8 +220,8 @@ describe("<MilestoneField>", () => {
       });
 
       it("displays the milestone percent completed", () => {
-        expect(milestone.findMilestonePercentCompleted()).toEqual("65")
-      })
+        expect(milestone.findMilestonePercentCompleted()).toEqual("65");
+      });
     });
 
     describe("Example 2", () => {
@@ -253,8 +253,80 @@ describe("<MilestoneField>", () => {
       });
 
       it("displays the milestone percent completed", () => {
-        expect(milestone.findMilestonePercentCompleted()).toEqual("80")
-      })
+        expect(milestone.findMilestonePercentCompleted()).toEqual("80");
+      });
+    });
+  });
+
+  describe("Changing pre-populated form data", () => {
+    describe("Example 1", () => {
+      let milestone;
+      let formData = {
+        description: "Here is a stone that marks a mile.",
+        milestoneBaselineCompletion: "31/03/2018",
+        milestoneSummaryOfCriticalPath: "Contract begins!!!",
+        statusAgainstLastReturn: "Completed",
+        currentReturn: "21/09/2019",
+        reasonForVariance: "A variance has occured, causing variance",
+        milestonePercentCompleted: "65"
+      };
+      let schemaTitle = "Milestone Fields";
+      milestone = new MilestoneComponent(formData, onChangeSpy, schemaTitle);
+
+      it("updates the value of the status against last return", () => {
+        milestone.simulateStatusAgainstLastReturn("On schedule");
+        expect(milestone.findStatusAgainstLastReturn()).toEqual("On schedule");
+      });
+
+      it("updates the value of the current return", () => {
+        milestone.simulateCurrentReturn("31/10/2019");
+        expect(milestone.findCurrentReturn()).toEqual("31/10/2019");
+      });
+
+      it("updates the value of reason for variance", () => {
+        milestone.simulateReasonForVariance("New variance!");
+        expect(milestone.findReasonForVariance()).toEqual("New variance!");
+      });
+
+      it("updates the value of milestone percent completed", () => {
+        milestone.simulateMilestonePercentCompleted("70");
+        expect(milestone.findMilestonePercentCompleted()).toEqual("70");
+      });
+    });
+
+    describe("Example 1", () => {
+      let milestone;
+      let formData = {
+        description: "Cats have walked very far",
+        milestoneBaselineCompletion: "10/10/2019",
+        milestoneSummaryOfCriticalPath: "Give them pets and fish",
+        statusAgainstLastReturn: "On schedule",
+        currentReturn: "25/12/2018",
+        reasonForVariance: "The cats are fast and are moving quickly.",
+        milestonePercentCompleted: "80"
+      };
+      let schemaTitle = "Cats walking club";
+      milestone = new MilestoneComponent(formData, onChangeSpy, schemaTitle);
+
+      it("updates the value of the status against last return", () => {
+        milestone.simulateStatusAgainstLastReturn("Completed");
+        expect(milestone.findStatusAgainstLastReturn()).toEqual("Completed");
+      });
+
+      it("updates the value of the current return", () => {
+        milestone.simulateCurrentReturn("01/01/2019");
+        expect(milestone.findCurrentReturn()).toEqual("01/01/2019");
+      });
+
+      it("updates the value of reason for variance", () => {
+        milestone.simulateReasonForVariance("More cats have started the journey");
+        expect(milestone.findReasonForVariance()).toEqual("More cats have started the journey");
+      });
+
+      it("updates the value of milestone percent completed", () => {
+        milestone.simulateMilestonePercentCompleted("99");
+        expect(milestone.findMilestonePercentCompleted()).toEqual("99");
+      });
     });
   });
 });
