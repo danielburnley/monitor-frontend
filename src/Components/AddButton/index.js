4a4bdacc1a8a99337ee86a59ac31fd11d20fff35
Christopher Parsons <mail@christopherparsons.me>
2018-11-07 14:46:56 +0000

    Remove useless constructors

--- src/Components/AddButton/index.js
+++ src/Components/AddButton/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 
 export default class AddButton extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div className="row">
