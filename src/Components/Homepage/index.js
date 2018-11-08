86d9ad6b3f64362680c5129d79861e040e505e4c
Christopher Parsons <mail@christopherparsons.me>
2018-11-08 14:26:37 +0000

    Rearrange elements of homepage

--- src/Components/Homepage/index.js
+++ src/Components/Homepage/index.js
@@ -1,20 +1,29 @@
-import runtimeEnv from '@mars/heroku-js-runtime-env';
+import runtimeEnv from "@mars/heroku-js-runtime-env";
 import "./style.css";
 import React from "react";
 
 export default class Homepage extends React.Component {
   constructor(props) {
-    super(props)
-    this.env = runtimeEnv()
+    super(props);
+    this.env = runtimeEnv();
   }
   render() {
     return (
-      <div className="container homepage">
-        <h1>Homes England Monitor</h1>
-        Welcome to the Homes England monitoring system.
-          If you have not been provided with a link to a project you believe you
-          should have access to, or have any feedback or queries please feel
-          free to email <a href={"mailto:"+this.env.REACT_APP_SUPPORT_EMAIL}>{this.env.REACT_APP_SUPPORT_EMAIL}</a>.
+      <div className="row homepage">
+        <div className="col-md-2" />
+        <div className="col-md-8">
+          <h1>Homes England Monitor</h1>
+          <p>
+            Welcome to the Homes England monitoring system. If you have not been
+            provided with a link to a project you believe you should have access
+            to, or have any feedback or queries please feel free to email{" "}
+            <a href={"mailto:" + this.env.REACT_APP_SUPPORT_EMAIL}>
+              {this.env.REACT_APP_SUPPORT_EMAIL}
+            </a>
+            .
+          </p>
+        </div>
+        <div className="col-md-2" />
       </div>
     );
   }
