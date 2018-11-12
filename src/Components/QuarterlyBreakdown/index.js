845f10646122076acd498d64d095c3696aed454f
Emma Corbett <emma@madetech.com>
2018-11-12 15:42:20 +0000

    Force to rerended after delete button

--- src/Components/QuarterlyBreakdown/index.js
+++ src/Components/QuarterlyBreakdown/index.js
@@ -120,6 +120,8 @@ export default class QuarterlyBreakdown extends React.Component {
     updatedArray.splice(index, 1);
 
     this.setState({ data: updatedArray });
+    
+    this.forceUpdate();
   }
 
   render() {
