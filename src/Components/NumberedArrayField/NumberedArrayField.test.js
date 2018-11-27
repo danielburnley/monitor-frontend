5108f559e3f8d5ca752463a7ce955d50cdbf62c2
Faissal Bensefia <faissal@madetech.com>
2018-11-27 12:13:53 +0000

    Add ability to add items to numbered array field

--- src/Components/NumberedArrayField/NumberedArrayField.test.js
+++ src/Components/NumberedArrayField/NumberedArrayField.test.js
@@ -4,9 +4,12 @@ import NumberedArrayField from ".";
 
 describe("Numbered Array Field Financials", () => {
   it("Doesn't render the submit button", async () => {
-    let wrapper = mount(<NumberedArrayField/>);
+    let wrapper = mount(<NumberedArrayField
+        formData={ [{otherData: "Hello"}] }
+        schema={ {items: {type: "object", properties: {otherData: {type: "string"}} } } }
+      />);
     await wrapper.update();
-    expect(wrapper.find("button").length).toEqual(0);
+    expect(wrapper.find("button[type='submit']").length).toEqual(0);
   });
 
   describe("renders all data", () => {
@@ -25,14 +28,35 @@ describe("Numbered Array Field Financials", () => {
     it("Example 2", async () => {
       let wrapper = mount(<NumberedArrayField
           formData={ [{otherData: "Hello"}, {otherData: "There"}] }
-          schema={ {items: {type: "object", properties: {otherData: {type: "string"}, moreData: {type: "string"}}}} }
+          schema={ {items: {type: "object", properties: {otherData: {type: "string"}, moreData: {type: "string"}}} } }
         />
       );
       await wrapper.update();
       expect(wrapper.find("Form").length).toEqual(2);
-      expect(wrapper.find("input#root_otherData").length).toEqual(2)
-      expect(wrapper.find("input#root_otherData").at(0).props().value).toEqual("Hello")
-      expect(wrapper.find("input#root_otherData").at(1).props().value).toEqual("There")
+      expect(wrapper.find("input#root_otherData").length).toEqual(2);
+      expect(wrapper.find("input#root_moreData").length).toEqual(2);
+      expect(wrapper.find("input#root_otherData").at(0).props().value).toEqual("Hello");
+      expect(wrapper.find("input#root_otherData").at(1).props().value).toEqual("There");
     });
+  });
+
+  it("Adds items", async () => {
+    let wrapper = mount(<NumberedArrayField
+        formData={ [{otherData: "Hello"}, {otherData: "There"}] }
+        schema={ {items: {type: "object", properties: {otherData: {type: "string"}}} }}
+      />
+    );
+    await wrapper.update();
+    wrapper.find('[data-test="add-button"]').simulate('click');
+    expect(wrapper.find("input#root_otherData").length).toEqual(3);
+  });
+
+  it("Removes items", async () => {
+    let wrapper = mount(<NumberedArrayField
+        formData={ [{otherData: "Hello"}, {otherData: "There"}] }
+        schema={ {items: {type: "object", properties: {otherData: {type: "string"}}} }}
+      />
+    );
+    await wrapper.update();
   });
 });
--- src/Components/NumberedArrayField/index.js
+++ src/Components/NumberedArrayField/index.js
@@ -13,11 +13,22 @@ export default class NumberedArrayField extends React.Component {
     };
   }
 
+  addItem = (e) => {
+    let formData = this.state.formData;
+    formData.push({});
+    this.setState({formData});
+  }
   render() {
-    return <ol>{
-        this.props.formData &&
-        this.props.formData.map((data, index) => <li key={index}><Form formData={data} schema={this.props.schema.items}><br/></Form></li>)
-      }</ol>;
+    return <div>
+          <ol>{
+            this.props.formData &&
+            this.props.formData.map((data, index) => <li key={index}>
+              <Form formData={data} schema={this.props.schema.items}><br/></Form>
+            </li>)
+          }
+        </ol>
+      <AddButton onClick={this.addItem} />
+    </div>;
     //Switch away from index to an id based on contents or something
     //return <ol>{this.props.formData.map((object, index) => <li key = {index}><Form formData={object} schema={this.props.schema.items} uiSchema={this.props.uiSchema}><br/></Form></li>)}</ol>
   }
