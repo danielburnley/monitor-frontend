f64befc28f6d1e23a5f89956fdb70e6e54bac751
Emma Corbett <emma@madetech.com>
2018-12-14 13:44:29 +0000

    Add dependency to risk field, and generalise titles.

--- src/Components/RiskField/index.js
+++ src/Components/RiskField/index.js
@@ -5,16 +5,16 @@ export default class RiskField extends React.Component {
     super(props);
 
     this.state = {
-      riskBaselineRisk: this.props.formData.riskBaselineRisk ,
+      riskBaselineRisk: this.props.formData.riskBaselineRisk,
       riskBaselineImpact: this.props.formData.riskBaselineImpact,
       riskBaselineLikelihood: this.props.formData.riskBaselineLikelihood,
-      riskBaselineMitigationsInPlace:
-        this.props.formData.riskBaselineMitigationsInPlace,
+      riskBaselineMitigationsInPlace: this.props.formData
+        .riskBaselineMitigationsInPlace,
       riskCurrentReturnLikelihood:
         this.props.formData.riskCurrentReturnLikelihood || "1",
       riskAnyChange: this.props.formData.riskAnyChange || "No",
-      riskCurrentReturnMitigationsInPlace:
-        this.props.formData.riskCurrentReturnMitigationsInPlace
+      riskCurrentReturnMitigationsInPlace: this.props.formData
+        .riskCurrentReturnMitigationsInPlace
     };
   }
 
@@ -27,8 +27,10 @@ export default class RiskField extends React.Component {
   renderRiskDescription = () => (
     <div>
       <h5>
-        <b data-test="risk-description-title" >{this.props.schema.properties.riskBaselineRisk && 
-          this.props.schema.properties.riskBaselineRisk.title}</b>
+        <b data-test="risk-description-title">
+          {this.props.schema.properties.riskBaselineRisk &&
+            this.props.schema.properties.riskBaselineRisk.title}
+        </b>
       </h5>
       <p data-test="risk-description">{this.props.formData.riskBaselineRisk}</p>
     </div>
@@ -36,16 +38,22 @@ export default class RiskField extends React.Component {
 
   renderRiskBaselineImpact = () => (
     <div className="col-md-3">
-      <h5 data-test="risk-impact-title">{this.props.schema.properties.riskBaselineImpact &&
-        this.props.schema.properties.riskBaselineImpact.title}:</h5>
+      <h5 data-test="risk-impact-title">
+        {this.props.schema.properties.riskBaselineImpact &&
+          this.props.schema.properties.riskBaselineImpact.title}
+        :
+      </h5>
       <p data-test="risk-impact">{this.props.formData.riskBaselineImpact}</p>
     </div>
   );
 
   renderRiskLikelihood = () => (
     <div className="col-md-3">
-      <h5 data-test="risk-likelihood-title">{this.props.schema.properties.riskBaselineLikelihood &&
-         this.props.schema.properties.riskBaselineLikelihood.title}:</h5>
+      <h5 data-test="risk-likelihood-title">
+        {this.props.schema.properties.riskBaselineLikelihood &&
+          this.props.schema.properties.riskBaselineLikelihood.title}
+        :
+      </h5>
       <p data-test="risk-likelihood">
         {this.props.formData.riskBaselineLikelihood}
       </p>
@@ -55,8 +63,10 @@ export default class RiskField extends React.Component {
   renderRiskMitigationInPlace = () => (
     <div className="col-md-6">
       <h5>
-        <b data-test="risk-mitigation-in-place-title">{this.props.schema.properties.riskBaselineMitigationsInPlace && 
-          this.props.schema.properties.riskBaselineMitigationsInPlace.title}</b>
+        <b data-test="risk-mitigation-in-place-title">
+          {this.props.schema.properties.riskBaselineMitigationsInPlace &&
+            this.props.schema.properties.riskBaselineMitigationsInPlace.title}
+        </b>
       </h5>
       <p data-test="risk-mitigation-in-place">
         {this.props.formData.riskBaselineMitigationsInPlace}
@@ -68,9 +78,13 @@ export default class RiskField extends React.Component {
     <div className="col-md-3">
       <div className="row">
         <div className="col-md-12">
-          <label htmlFor="currentReturnLikelihood" data-test="risk-current-likelihood-title">
-          {this.props.schema.properties.riskCurrentReturnLikelihood &&
-            this.props.schema.properties.riskCurrentReturnLikelihood.title}{" "}
+          <label
+            htmlFor="currentReturnLikelihood"
+            data-test="risk-current-likelihood-title"
+          >
+            {this.props.schema.properties.riskCurrentReturnLikelihood &&
+              this.props.schema.properties.riskCurrentReturnLikelihood
+                .title}{" "}
           </label>
         </div>
         <div row="row">
@@ -100,8 +114,13 @@ export default class RiskField extends React.Component {
     <div className="col-md-3">
       <div className="row">
         <div className="col-md-12">
-          <label htmlFor="anyChangeInRisk" data-test="risk-change-in-risk-title">{this.props.schema.properties.riskAnyChange &&
-            this.props.schema.properties.riskAnyChange.title} </label>
+          <label
+            htmlFor="anyChangeInRisk"
+            data-test="risk-change-in-risk-title"
+          >
+            {this.props.schema.properties.riskAnyChange &&
+              this.props.schema.properties.riskAnyChange.title}{" "}
+          </label>
         </div>
       </div>
       <div className="row">
@@ -121,21 +140,30 @@ export default class RiskField extends React.Component {
     </div>
   );
 
-  renderCurrentMitigationsInPlace = () => (
-    <div className="col-md-6">
-      <label htmlFor="currentReturnMitigationInPlace" data-test="risk-current-mitigations-in-place-title">
-      {this.props.schema.properties.riskCurrentReturnMitigationsInPlace &&
-        this.props.schema.properties.riskCurrentReturnMitigationsInPlace.title} 
-      </label>
-      <textarea
-        className="form-control"
-        id="currentReturnMitigationInPlace"
-        onChange={e => this.onFieldChange("riskCurrentReturnMitigationsInPlace", e)}
-        data-test="risk-current-mitigations-in-place"
-        value={this.state.riskCurrentReturnMitigationsInPlace}
-      />
-    </div>
-  );
+  renderCurrentMitigationsInPlace = () => {
+    if (this.state.riskAnyChange !== "Yes") return null;
+    return (
+      <div className="col-md-6">
+        <label
+          htmlFor="currentReturnMitigationInPlace"
+          data-test="risk-current-mitigations-in-place-title"
+        >
+          {this.props.schema.properties.riskCurrentReturnMitigationsInPlace &&
+            this.props.schema.properties.riskCurrentReturnMitigationsInPlace
+              .title}
+        </label>
+        <textarea
+          className="form-control"
+          id="currentReturnMitigationInPlace"
+          onChange={e =>
+            this.onFieldChange("riskCurrentReturnMitigationsInPlace", e)
+          }
+          data-test="risk-current-mitigations-in-place"
+          value={this.state.riskCurrentReturnMitigationsInPlace}
+        />
+      </div>
+    );
+  };
 
   renderBody = () => (
     <div className="panel-body">
@@ -162,7 +190,9 @@ export default class RiskField extends React.Component {
   render() {
     return (
       <div className="panel panel-default">
-        <div className="panel-heading" data-test="schema-title">{this.props.schema.title}</div>
+        <div className="panel-heading" data-test="schema-title">
+          {this.props.schema.title}
+        </div>
         {this.renderBody()}
       </div>
     );
