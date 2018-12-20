bdcf9dbe76da8a2d896e4536602680a71a4b1116
Christopher Parsons <mail@christopherparsons.me>
2018-12-20 12:23:28 +0000

    Add submitted timestamp to rendering of the return list

--- src/Components/ReturnList/returnList.test.js
+++ src/Components/ReturnList/returnList.test.js
@@ -13,6 +13,10 @@ class ReturnListComponent {
     this.returnList.find(`[data-test='status-${number}']`).text();
   urlTarget = number =>
     this.returnList.find(`[data-test='url-${number}']`).prop("href");
+  timestamp = number =>
+    this.returnList.find(`[data-test='timestamp-${number}']`).text();
+  timestampExists = number =>
+    this.returnList.find(`[data-test='timestamp-${number}']`).length;
 }
 
 describe("<ReturnList", () => {
@@ -27,7 +31,8 @@ describe("<ReturnList", () => {
             {
               changes: "Yes"
             }
-          ]
+          ],
+          timestamp: 1545739200
         }
       ];
       let returnList = new ReturnListComponent(returns);
@@ -47,6 +52,10 @@ describe("<ReturnList", () => {
       it("shows the current status of the return", () => {
         expect(returnList.status(7)).toEqual("Draft");
       });
+
+      it("shows the submission time of the return", () => {
+        expect(returnList.timestamp(7)).toEqual("25/12/2018");
+      });
     });
 
     describe("Example 2", () => {
@@ -59,7 +68,8 @@ describe("<ReturnList", () => {
             {
               changes: "Some"
             }
-          ]
+          ],
+          timestamp: 0
         }
       ];
       let returnList = new ReturnListComponent(returns);
@@ -79,33 +89,39 @@ describe("<ReturnList", () => {
       it("shows the current status of the return", () => {
         expect(returnList.status(7)).toEqual("Saved");
       });
+
+      it("does not display the timestamp", () => {
+        expect(returnList.timestampExists(7)).toEqual(0);
+      });
     });
   });
 
   describe("Given two returns", () => {
     describe("Example 1", () => {
       let returns = [
-          {
-            id: 1,
-            project_id: 1,
-            status: "Draft",
-            updates: [
-              {
-                changes: "Yes"
-              }
-            ]
-          },
-          {
-            id: 2,
-            project_id: 1,
-            status: "Saved",
-            update: [
-              {
-                changes: "Some"
-              }
-            ]
-          }
-        ];
+        {
+          id: 1,
+          project_id: 1,
+          status: "Draft",
+          updates: [
+            {
+              changes: "Yes"
+            }
+          ],
+          timestamp: 1545308331
+        },
+        {
+          id: 2,
+          project_id: 1,
+          status: "Saved",
+          update: [
+            {
+              changes: "Some"
+            }
+          ],
+          timestamp: 0
+        }
+      ];
       let returnList = new ReturnListComponent(returns);
 
       it("displays this schema title", () => {
@@ -126,31 +142,38 @@ describe("<ReturnList", () => {
         expect(returnList.status(1)).toEqual("Draft");
         expect(returnList.status(2)).toEqual("Saved");
       });
+
+      it("shows the timestamp when relevant", () => {
+        expect(returnList.timestamp(1)).toEqual("20/12/2018");
+        expect(returnList.timestampExists(2)).toEqual(0);
+      });
     });
 
     describe("Example 2", () => {
       let returns = [
-          {
-            id: 7,
-            project_id: 1,
-            status: "Saved",
-            updates: [
-              {
-                changes: "Some"
-              }
-            ]
-          },
-          {
-            id: 6,
-            project_id: 1,
-            status: "Woof",
-            update: [
-              {
-                changes: "Meow"
-              }
-            ]
-          }
-        ];
+        {
+          id: 7,
+          project_id: 1,
+          status: "Saved",
+          updates: [
+            {
+              changes: "Some"
+            }
+          ],
+          timestamp: 1546344000
+        },
+        {
+          id: 6,
+          project_id: 1,
+          status: "Woof",
+          update: [
+            {
+              changes: "Meow"
+            }
+          ],
+          timestamp: 1514808000
+        }
+      ];
       let returnList = new ReturnListComponent(returns);
 
       it("displays this schema title", () => {
@@ -170,6 +193,11 @@ describe("<ReturnList", () => {
       it("shows the current status of the return", () => {
         expect(returnList.status(7)).toEqual("Saved");
         expect(returnList.status(6)).toEqual("Woof");
+      });
+
+      it("shows the timestamp", () => {
+        expect(returnList.timestamp(7)).toEqual("1/1/2019");
+        expect(returnList.timestamp(6)).toEqual("1/1/2018");
       });
     });
   });
