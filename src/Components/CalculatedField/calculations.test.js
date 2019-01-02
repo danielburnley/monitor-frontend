2a09562fd5f651dc422c73cf15b8f0ea55166ffb
Faissal Bensefia <faissal@madetech.com>
2019-01-02 10:48:50 +0000

    Add tests for parseMoney and accumulateMoney

---
+++ src/Components/CalculatedField/calculations.test.js
@@ -0,0 +1,26 @@
+import {parseMoney, accumulateMoney} from ".";
+describe("parseMoney()", () => {
+  it("Returns 0 when given no value", () => {
+    expect(parseMoney()).toEqual(0);
+  });
+
+  describe("Parses a formatted money value", () => {
+    it("Example 1", () => {
+      expect(parseMoney("$6.28")).toEqual(6.28);
+    });
+
+    it("Example 2", () => {
+      expect(parseMoney("฿3.14")).toEqual(3.14);
+    });
+  });
+});
+
+describe("accumulateMoney()", () => {
+  describe("Sums up the values in an array with properties", () => {
+    it("Example 1", () => {
+      expect(
+        accumulateMoney([{operand: "2"}, {operand: "4"}, {operand: "8"}], 'operand')
+      ).toEqual(14);
+    });
+  })
+});
--- src/Components/CalculatedField/index.js
+++ src/Components/CalculatedField/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function parseMoney(value) {
+export function parseMoney(value) {
   if (value) {
     let cleanedString = value.replace(/[^0-9.]/g, "");
     return new Number(cleanedString);
@@ -8,19 +8,19 @@ function parseMoney(value) {
   return 0;
 }
 
-function accumulateMoney(array, property) {
+export function accumulateMoney(array, property) {
   return array.reduce(
     (total, object) => parseMoney(object[property]) + total,
     0
   );
 }
 
-function sum(data, ...keys) {
+export function sum(data, ...keys) {
   keys = keys.flat();
   return keys.reduce((total, key) => parseMoney(data[key]) + total, 0);
 }
 
-function periodTotal(object, totalPath, property, ...keys) {
+export function periodTotal(object, totalPath, property, ...keys) {
   if(!object[property]) return;
   return object[property].forEach((value, index) => {
     setCreate(object[property][index], totalPath, ""+sum(value, keys));
@@ -28,17 +28,17 @@ function periodTotal(object, totalPath, property, ...keys) {
   });
 }
 
-function weeksPassed(originalDate, newDate) {
+export function weeksPassed(originalDate, newDate) {
   let seconds = secondsPassed(originalDate, newDate)
   return Math.round(seconds/(7 * 60 * 60 * 24));
 }
 
-function daysPassed(originalDate, newDate) {
+export function daysPassed(originalDate, newDate) {
   let seconds = secondsPassed(originalDate, newDate)
   return Math.round(seconds/(60 * 60 * 24));
 }
 
-function secondsPassed(originalDate, newDate) {
+export function secondsPassed(originalDate, newDate) {
   let originalDateDatified = new Date(originalDate);
   let newDateDatified = new Date(newDate);
 
@@ -47,13 +47,13 @@ function secondsPassed(originalDate, newDate) {
   return (newDateDatified - originalDateDatified) / 1000
 }
 
-function validateArrayPropertyIsLessThan(array, path, value) {
+export function validateArrayPropertyIsLessThan(array, path, value) {
   array.forEach((object) => {
     setCreate(object, [...path.slice(0, path.length-1), '_valid'], get(object, ...path) <= value);
   });
 }
 
-function setCreate(object, path, value) {
+export function setCreate(object, path, value) {
   let jsonToSet = path.slice(0,path.length-1).reduce(
     (accumulator, property) => {
       if (accumulator[property]) {
@@ -70,11 +70,11 @@ function setCreate(object, path, value) {
   return object;
 }
 
-function set(object, property, value) {
+export function set(object, property, value) {
   object[property] = value;
 }
 
-function get(object, ...properties) {
+export function get(object, ...properties) {
   return properties.reduce(
     (accumulator, property) => {
       if (accumulator && accumulator[property])
@@ -88,13 +88,13 @@ function get(object, ...properties) {
   );
 }
 
-function calculateVariance(value1, value2) {
+export function calculateVariance(value1, value2) {
   let result = (value1 / value2) * 100;
 
   return Math.round(result * 100) / 100;
 }
 
-function combineArrays(array1, array2) {
+export function combineArrays(array1, array2) {
   if (!array1) return array2;
   if (!array2) return array1;
   return array1.concat(array2)
