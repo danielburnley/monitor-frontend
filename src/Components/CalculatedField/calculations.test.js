e9fd64a6fbef07f046a42f956a24e9f1e41c8933
Faissal Bensefia <faissal@madetech.com>
2019-01-02 12:29:16 +0000

    Test periodTotal()

--- src/Components/CalculatedField/calculations.test.js
+++ src/Components/CalculatedField/calculations.test.js
@@ -11,7 +11,8 @@ import {
   daysPassed,
   secondsPassed,
   setCreate,
-  validateArrayPropertyIsLessThan
+  validateArrayPropertyIsLessThan,
+  periodTotal
 } from ".";
 
 describe("parseMoney()", () => {
@@ -216,6 +217,49 @@ describe("validateArrayPropertyIsLessThan()", () => {
       let formData = [{main: "24"}];
       validateArrayPropertyIsLessThan(formData, ["main"], 19);
       expect(formData).toEqual([{"_valid": false, "main": "24"}]);
+    });
+  });
+});
+
+describe("periodTotal()", () => {
+  describe("With data", () => {
+    it("Example 1", () => {
+      let formData = {periods: [{Q1: "12", Q2: "8"}]};
+      periodTotal(formData, ["total"], "periods", "Q1", "Q2");
+      expect(formData).toEqual(
+        {
+          "periods": [
+            {"Q1": "12", "Q2": "8", "total": "20"}
+          ]
+        }
+      );
+    });
+
+    it("Example 2", () => {
+      let formData = {per: [{Q3: "12", Q4: "8"}, {Q3: "32", Q4: "16"}]};
+      periodTotal(formData, ["value"], "per", "Q3", "Q4");
+      expect(formData).toEqual(
+        {
+          "per": [
+            {"Q3": "12", "Q4": "8", "value": "20"},
+            {"Q3": "32", "Q4": "16", "value": "48"}
+          ]
+        }
+      );
+    });
+  });
+
+  describe("Without data", () => {
+    it("Example 1", () => {
+      let formData = {};
+      periodTotal(formData, ["total"], "periods", "Q1", "Q2");
+      expect(formData).toEqual({});
+    });
+
+    it("Example 2", () => {
+      let formData = {};
+      periodTotal(formData, ["value"], "per", "Q3", "Q4");
+      expect(formData).toEqual({});
     });
   });
 });
