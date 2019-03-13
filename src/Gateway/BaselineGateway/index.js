3f9f908a33bec580d559139ef6225479924d0790
Emma Corbett <emma@madetech.com>
2019-03-13 16:54:01 +0000

    Add baseline gateway

---
+++ src/Gateway/BaselineGateway/index.js
@@ -0,0 +1,83 @@
+import Project from "../../Domain/Project";
+import fetch from "isomorphic-fetch";
+import runtimeEnv from "@mars/heroku-js-runtime-env";
+
+export default class BaselineGateway {
+  constructor(apiKeyGateway) {
+    this.apiKeyGateway = apiKeyGateway;
+    this.env = runtimeEnv();
+  }
+
+  async submit(baseline_id) {
+    let response = await fetch(
+      `${this.env.REACT_APP_HIF_API_URL}baseline/submit`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          API_KEY: this.apiKeyGateway.getApiKey().apiKey
+        },
+        body: JSON.stringify({
+          baseline_id
+        })
+      }
+    ).catch(() => ({ ok: false }));
+
+    if (response.ok) {
+      return { success: true };
+    } else {
+      return { success: false };
+    }
+  }
+
+  async amend(project_id, data, timestamp) {
+    let response = await fetch(
+      `${this.env.REACT_APP_HIF_API_URL}baseline/amend`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          API_KEY: this.apiKeyGateway.getApiKey().apiKey
+        },
+        body: JSON.stringify({
+          project_id,
+          data,
+          timestamp
+        })
+      }
+    ).catch(() => ({ ok: false }))
+
+    if (response.ok) {
+      let rawResponse = await response.json();
+      return {
+        success: true,
+        baselineId: rawResponse.baselineId,
+        errors: rawResponse.errors
+      }
+    } else {
+      return { success: false }
+    }
+  }
+
+  async getAllBaselines(project_id) {
+    let response = await fetch(
+      `${this.env.REACT_APP_HIF_API_URL}project/${project_id}/baselines`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          API_KEY: this.apiKeyGateway.getApiKey().apiKey
+        }
+      }
+    ).catch(() => ({ ok: false }))
+
+    if (response.ok) {
+      let rawResponse = await response.json();
+      return {
+        success: true,
+        baselines: rawResponse.baselines
+      }
+    } else {
+      return { success: false }
+    }
+  }
+}
